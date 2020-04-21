1df3bc0021d3ee9d3a5f4e706a392486cf4a8e48
Violet Bora Lee <learnerbr@gmail.com>
2020-04-22 03:35:27 +0900

    todo 등록하기 (POST)

--- routes/todos.js
+++ routes/todos.js
@@ -16,6 +16,40 @@ router.get('/', async (req, res) => {
 	}
 })
 
+// @route			POST api/todos
+// @desc			Add new todos
+router.post('/', [
+		check('title', '제목을 입력해주세요.').not().isEmpty(),
+		check('title', '제목이 너무 깁니다. 20자 이내로 줄여주세요.').isLength({max: 20})
+	],
+	async (req, res) => {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			return res.status(400).json({errors: errors.array()})
+		}
+
+		const {title, description, deadline, priority, done} = req.body;
+
+		try {
+			const newTodo = new Todo({
+				title,
+				description,
+				deadline,
+				priority,
+				done
+			});
+
+			const todo = await newTodo.save();
+
+			res.json(todo);
+
+		} catch (err) {
+			console.error(err.message);
+			res.status(500).send('Server Error');
+		}
+	}
+);
+
 module.exports = router;
 
 
