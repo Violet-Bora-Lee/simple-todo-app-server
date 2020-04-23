273d2c58487ffa12f1a35323d3a56bb6b8797fb9
Violet Bora Lee <learnerbr@gmail.com>
2020-04-24 01:11:50 +0900

    heroku 배포 설정

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express();
 
@@ -11,6 +12,14 @@ app.use(express.json( { extended: false }));
 
 // Define Routes
 app.use('/api/todos', require('./routes/todos'));
+
+// Sever static assets in production
+if(process.env.NODE_ENV === 'production') {
+	// Set static folder
+	app.use(express.static('client/build'));
+
+	app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')))
+}
 
 const PORT = process.env.PORT || 6000;
 
